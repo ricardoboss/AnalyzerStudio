58e94e9b89b50ceed3797fdcc89d961cfbf6d263
Ricardo Boss <contact@ricardoboss.de>
2020-06-18 01:21:14 +0200

    Update analysis if project gets dirty

--- UtilityAnalyzerStudio/Models/AnalysisProject.cs
+++ UtilityAnalyzerStudio/Models/AnalysisProject.cs
@@ -1,4 +1,4 @@
-﻿using Newtonsoft.Json;
+using Newtonsoft.Json;
 
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -10,172 +10,182 @@ using System.Text.Json;
 
 namespace UtilityAnalyzerStudio.Models
 {
-    public class AnalysisProject : BaseModel
-    {
-        private static readonly JsonSerializerSettings settings = new JsonSerializerSettings()
-        {
-            Formatting = Formatting.Indented,
-            NullValueHandling = NullValueHandling.Ignore
-        };
+	public class AnalysisProject : BaseModel
+	{
+		private static readonly JsonSerializerSettings settings = new JsonSerializerSettings()
+		{
+			Formatting = Formatting.Indented,
+			NullValueHandling = NullValueHandling.Ignore
+		};
 
-        public static AnalysisProject NewAt(string name, string path)
-        {
-            var project = new AnalysisProject(name)
-            {
-                Path = path,
-                IsDirty = true
-            };
+		public static AnalysisProject NewAt(string name, string path)
+		{
+			var project = new AnalysisProject(name)
+			{
+				Path = path,
+				IsDirty = true
+			};
 
-            return project;
-        }
+			return project;
+		}
 
-        public static void SaveAt(AnalysisProject project, string path)
-        {
-            var json = JsonConvert.SerializeObject(project, settings);
-            File.WriteAllText(path, json);
+		public static void SaveAt(AnalysisProject project, string path)
+		{
+			var json = JsonConvert.SerializeObject(project, settings);
+			File.WriteAllText(path, json);
 
-            project.Path = path;
-            project.IsDirty = false;
-        }
+			project.Path = path;
+			project.IsDirty = false;
+		}
 
-        public static AnalysisProject OpenFrom(string path)
-        {
-            var json = File.ReadAllText(path);
-            var project = JsonConvert.DeserializeObject<AnalysisProject>(json, settings);
-            project.Path = path;
+		public static AnalysisProject OpenFrom(string path)
+		{
+			var json = File.ReadAllText(path);
+			var project = JsonConvert.DeserializeObject<AnalysisProject>(json, settings);
+			project.Path = path;
 
-            return project;
-        }
+			return project;
+		}
 
-        [JsonIgnore]
-        public bool IsDirty
-        {
-            get => isDirty;
-            private set => SetProperty(ref isDirty, value, nameof(IsDirty), nameof(ProjectTitle));
-        }
-        private bool isDirty;
+		[JsonIgnore]
+		public bool IsDirty
+		{
+			get => isDirty;
+			private set
+			{
+				SetProperty(ref isDirty, value, nameof(IsDirty), nameof(ProjectTitle));
 
-        [JsonIgnore]
-        public string Path
-        {
-            get => path;
-            private set => SetProperty(ref path, value, nameof(Path), nameof(ProjectTitle));
-        }
-        private string path;
+				UpdateAnalysis();
+			}
+		}
+		private bool isDirty;
 
-        [JsonIgnore]
-        public string ProjectTitle => $"{Name}{(IsDirty ? "*" : "")} ({Path})";
+		[JsonIgnore]
+		public string Path
+		{
+			get => path;
+			private set => SetProperty(ref path, value, nameof(Path), nameof(ProjectTitle));
+		}
+		private string path;
 
-        [JsonProperty]
-        public string Name
-        {
-            get => name;
-            set
-            {
-                SetProperty(ref name, value, nameof(Name), nameof(ProjectTitle));
-                IsDirty = true;
-            }
-        }
-        private string name;
+		[JsonIgnore]
+		public string ProjectTitle => $"{Name}{(IsDirty ? "*" : "")} ({Path})";
 
-        [JsonProperty]
-        public ObservableCollection<Property> Properties { get; } = new ObservableCollection<Property>();
+		[JsonProperty]
+		public string Name
+		{
+			get => name;
+			set
+			{
+				SetProperty(ref name, value, nameof(Name), nameof(ProjectTitle));
+				IsDirty = true;
+			}
+		}
+		private string name;
 
-        [JsonProperty]
-        public ObservableCollection<Specimen> Specimens { get; } = new ObservableCollection<Specimen>();
+		[JsonProperty]
+		public ObservableCollection<Property> Properties { get; } = new ObservableCollection<Property>();
 
-        [JsonIgnore]
-        public ObservableCollection<AnalysisDataset> Datasets { get; } = new ObservableCollection<AnalysisDataset>();
+		[JsonProperty]
+		public ObservableCollection<Specimen> Specimens { get; } = new ObservableCollection<Specimen>();
 
-        private AnalysisProject(string name)
-        {
-            Name = name;
+		[JsonIgnore]
+		public ObservableCollection<AnalysisDataset> Datasets { get; } = new ObservableCollection<AnalysisDataset>();
 
-            Properties.CollectionChanged += Properties_CollectionChanged;
-            Specimens.CollectionChanged += Specimens_CollectionChanged;
-        }
+		private AnalysisProject(string name)
+		{
+			Name = name;
 
-        [JsonConstructor]
-        public AnalysisProject(string name, IEnumerable<Property> properties, IEnumerable<Specimen> specimens) : this(name)
-        {
-            foreach (var p in properties)
-                Properties.Add(p);
+			Properties.CollectionChanged += Properties_CollectionChanged;
+			Specimens.CollectionChanged += Specimens_CollectionChanged;
+		}
 
-            foreach (var s in specimens)
-                Specimens.Add(s);
+		[JsonConstructor]
+		public AnalysisProject(string name, IEnumerable<Property> properties, IEnumerable<Specimen> specimens) : this(name)
+		{
+			foreach (var p in properties)
+				Properties.Add(p);
 
-            IsDirty = false;
-        }
+			foreach (var s in specimens)
+				Specimens.Add(s);
 
-        private void Properties_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
-        {
-            IsDirty = true;
+			IsDirty = false;
+		}
 
-            if (e.NewItems != null)
-                foreach (Property p in e.NewItems)
-                    p.PropertyChanged += Property_PropertyChanged;
+		private void Properties_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+		{
+			if (e.NewItems != null)
+				foreach (Property p in e.NewItems)
+					p.PropertyChanged += Property_PropertyChanged;
 
-            if (e.OldItems != null)
-                foreach (Property p in e.OldItems)
-                    p.PropertyChanged -= Property_PropertyChanged;
-        }
+			if (e.OldItems != null)
+				foreach (Property p in e.OldItems)
+				{
+					p.PropertyChanged -= Property_PropertyChanged;
 
-        private void Specimens_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
-        {
-            IsDirty = true;
+					foreach (var s in Specimens)
+						s.Properties.Remove(p.Name);
+				}
 
-            if (e.NewItems != null)
-                foreach (Specimen s in e.NewItems)
-                    s.PropertyChanged += Specimen_PropertyChanged;
+			IsDirty = true;
+		}
 
-            if (e.OldItems != null)
-                foreach (Specimen s in e.NewItems)
-                    s.PropertyChanged += Specimen_PropertyChanged;
-        }
+		private void Specimens_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+		{
+			if (e.NewItems != null)
+				foreach (Specimen s in e.NewItems)
+					s.PropertyChanged += Specimen_PropertyChanged;
 
-        private void Property_PropertyChanged(object sender, PropertyChangedEventArgs e)
-        {
-            IsDirty = true;
-        }
+			if (e.OldItems != null)
+				foreach (Specimen s in e.NewItems)
+					s.PropertyChanged += Specimen_PropertyChanged;
 
-        private void Specimen_PropertyChanged(object sender, PropertyChangedEventArgs e)
-        {
-            IsDirty = true;
-        }
+			IsDirty = true;
+		}
 
-        public void Save(string path = null)
-        {
-            SaveAt(this, path ?? Path);
-        }
+		private void Property_PropertyChanged(object sender, PropertyChangedEventArgs e)
+		{
+			IsDirty = true;
+		}
 
-        public void UpdateAnalysis()
-        {
-            var seenSpecimen = new List<Specimen>();
+		private void Specimen_PropertyChanged(object sender, PropertyChangedEventArgs e)
+		{
+			IsDirty = true;
+		}
 
-            foreach (var ds in Datasets)
-            {
-                ds.UpdateValue(Specimens, Properties);
+		public void Save(string path = null)
+		{
+			SaveAt(this, path ?? Path);
+		}
 
-                seenSpecimen.Add(ds.Specimen);
-            }
+		public void UpdateAnalysis()
+		{
+			var seenSpecimen = new List<Specimen>();
 
-            foreach (var specimen in Specimens.Where(s => !seenSpecimen.Contains(s)))
-            {
-                var ds = new AnalysisDataset(specimen);
+			foreach (var ds in Datasets)
+			{
+				ds.UpdateValue(Specimens, Properties);
 
-                ds.UpdateValue(Specimens, Properties);
+				seenSpecimen.Add(ds.Specimen);
+			}
 
-                Datasets.Add(ds);
-            }
+			foreach (var specimen in Specimens.Where(s => !seenSpecimen.Contains(s)))
+			{
+				var ds = new AnalysisDataset(specimen);
 
-            foreach (var specimen in seenSpecimen.Where(s => !Specimens.Contains(s)))
-            {
-                var ds = Datasets.First(d => d.Specimen == specimen);
-                if (ds == null)
-                    continue;
+				ds.UpdateValue(Specimens, Properties);
 
-                Datasets.Remove(ds);
-            }
-        }
-    }
+				Datasets.Add(ds);
+			}
+
+			foreach (var specimen in seenSpecimen.Where(s => !Specimens.Contains(s)))
+			{
+				var ds = Datasets.First(d => d.Specimen == specimen);
+				if (ds == null)
+					continue;
+
+				Datasets.Remove(ds);
+			}
+		}
+	}
 }
